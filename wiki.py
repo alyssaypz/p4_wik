798abd8cb57d3a716e245bf98bd4cc1225b5a68f
Alyssa Yepez <alyssayepez@yahoo.com>
2024-10-08 18:58:17 +0000

    Wikipedia

--- wiki.py
+++ wiki.py
@@ -1,6 +1,6 @@
 import wikipediaapi
 import time
-
+import queue as Queue
 
 user_agent = "p4_wik (alyssayepez@yahoo.com)"
 wiki = wikipediaapi.Wikipedia(user_agent, "en")
@@ -15,8 +15,48 @@ def fetch_links(page):
     return links_list 
 
 
+def wikipedia_solver(start_page,target_page):
+    print("working on it...")
+    start_time = time.time()
+
+    visited = set() 
+    queue = Queue()
+    parent = {}
+
+    while not queue.empty():
+        current_page_title = queue.get()
+        if current_page_title == target_page.title: 
+            break 
+
+        current_page= wiki.page(current_page_title)
+        links = fetch_links(current_page) 
+
+        for link in links: 
+            if link not in visited: 
+                queue.put(link)
+                visited.add(link)
+                parent[link] = current_page_title 
+
+path = []
+page_title = target_page.title 
+while page_title != start_page.title:
+    path.append(page_title) 
+    page_title = parent[page_title]
+    path.append(start_page_title) 
+    path.reverse()
+    end_time = time.time()
+    print("this took", end_time - start_time, "second to run") 
+return path
+
+
+
+
+
 #creating start and target pages 
 start_page = wiki.page("Pasadena High School (California)") 
-target_page = wiki.page("Rose Parade")
+target_page = wiki.page("Honda") 
+path = wikipedia_solver(start_page, target_page)
+print(path)
 
-print(fetch_links(start_page))
+
+
